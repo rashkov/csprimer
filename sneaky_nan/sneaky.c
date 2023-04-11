4bdd24a72cb4261e8f7d924d203ebf1a694f2717
Mike Rashkovsky <mrashkovsky@backtrace.io>
2023-04-10 20:30:25 -0400

    sneakny nan: convert to 64-bit

--- sneaky_nan/sneaky.c
+++ sneaky_nan/sneaky.c
@@ -4,16 +4,18 @@
 #include <assert.h>
 #include <stdlib.h>
 
-uint32_t NAN_MASK = 0xFF800000;
+// could go as low as 0xFFF0... but we can't fit a char into 4 bytes anyway
+uint64_t NAN_MASK = 0xFFFF000000000000;
 
-void encode(char * str, float * nan){
-  uint32_t * nan_u = (uint32_t *) nan;
+void encode(char * str, double * nan){
+  uint64_t * nan_u = (uint64_t *) nan;
+  uint64_t * str_u = (uint64_t *) str;
   *nan_u = *nan_u & NAN_MASK; // clear low order bits
-  *nan_u = *nan_u ^ *((uint32_t *) str);
+  *nan_u = *nan_u | *str_u;
 }
 
-void decode(float * nan){
-  uint32_t decoded = *((uint32_t *) nan) & 0x000FFFFF; // grab low-order bits
+void decode(double * nan){
+  uint64_t decoded = *((uint64_t *) nan) & 0x0000FFFFFFFFFFFF; // grab low-order bits
   printf("decoded: %s", &decoded);
 }
 
@@ -22,12 +24,13 @@ int main(int argc, char ** argv){
     printf("Usage ./sneaky <string>");
     exit(1);
   }
+
   char * str = argv[1];
   printf("Input string: %s\n", str);
-  uint32_t nan = NAN_MASK + 0x1;
+  uint64_t nan = NAN_MASK + 0x1;
   union {
-      float f;
-      uint32_t u;
+      double f;
+      uint64_t u;
   } u2f = { .u = nan };
 
   assert(isnan(u2f.f));
