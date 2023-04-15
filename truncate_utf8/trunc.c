d3a9ca32d1255f1d1cd3afc2cb94555d9321e637
Mike Rashkovsky <mrashkovsky@backtrace.io>
2023-04-15 11:29:59 -0400

    Trunc utf-8: almost complete.. doesn't print final newline

--- truncate_utf8/trunc.c
+++ truncate_utf8/trunc.c
@@ -8,40 +8,80 @@
 #include <stdint.h>
 
 
+char * INPUT = "./cases";
+
+char UTF_LEN_2_MASK = 0xC0; // 110x xxxx
+char UTF_LEN_3_MASK = 0xE0; // 1110 xxxx
+char UTF_LEN_4_MASK = 0xF0; // 1111 0xxx
+
+int read_one(int fd, void * buf){
+  if(read(fd, buf, 1) != 1) exit(0);
+  return 1;
+}
+
+void read_through_newline(int fd, char * buf){
+  while(read_one(fd, buf) && *buf != '\n'){};
+}
+
 int main(int argc, char **argv){
-
-  int fd = open("./cases", O_RDONLY);
-  char * buf = calloc(sizeof(char), 1);
-
+  int fd;
   char c;
   uint8_t n;
-  read(fd, &n, 1);
+
+  fd = open(INPUT, O_RDONLY);
+  read_one(fd, &n);
+
   for(; n >= 0; n--){
-    //printf("%u\n", n);
-    // read in a chracter
-    if(read(fd, &c, 1) != 1) exit(-1);
-    printf("%c", c);
-    // check if we hit a newline
-    if(c == '\n'){
-      // read in the next number
-      if(read(fd, &n, 1) != 1) exit(-1);
-      //printf("%u\n", n);
-      continue;
-    } else if(n == 0 && 0x80 & c){
-      // it's time to cut off but we're in a unicode sequence
-      printf("unicode");
-    } else if(n == 0) {
-      // read through newline
-      while(read(fd, &c, 1) == 1 && c != '\n');
-      // TODO: error above? what if we get a short read?
-      printf("\n");
-      // read in a new n
-      if(read(fd, &n, 1) != 1) exit(-1);
-      continue;
+    // read in a character
+    read_one(fd, &c);
+    if (c == '\n') {
+      read_one(fd, &n);
     }
+    if ((c & UTF_LEN_4_MASK) == UTF_LEN_4_MASK && n < 3){
+      read_through_newline(fd, &c);
+      read_one(fd, &n);
+    }
+    if ((c & UTF_LEN_3_MASK) == UTF_LEN_3_MASK && n < 2){
+      read_through_newline(fd, &c);
+      read_one(fd, &n);
+    }
+    if ((c & UTF_LEN_2_MASK) == UTF_LEN_2_MASK && n < 1){
+      read_through_newline(fd, &c);
+      read_one(fd, &n);
+    }
+    if (n == 0){
+      printf("%c", c);
+      read_through_newline(fd, &c);
+      read_one(fd, &n);
+    }
+    // if newline:
+    //   print it
+    //   read(n)
+    //   continue
+    // if unicode_4
+    //   if n < 3
+    //     read through newline
+    //     continue
+    //   else
+    //     print it
+    // if unicode_3
+    //   if n < 2
+    //     read through newline
+    //     continue
+    //   else
+    //     print it
+    // if unicode_2
+    //   if n < 1
+    //     read through newline
+    //     continue
+    //   else
+    //     print it
+    // if n == 0 // time to truncate
+    //   read through newline
+    //   continue
+    // else
+    //   print it
+
+    printf("%c", c);
   }
-  //printf("%d", n);
-  //while(read(fd, buf, 1) == 1){
-  //  printf("%s", buf);
-  //}
 }
