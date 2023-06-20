3057991e347ed6b94ac57ca31cc7f770817da66f
Mike Rashkovsky <mrashkovsky@backtrace.io>
2023-06-20 12:13:14 -0400

    pcap: Add packet header, needs testing

--- pcap/pcap.c
+++ pcap/pcap.c
@@ -16,6 +16,13 @@ typedef struct pcap_hdr_s {
   guint32 network;        /* data link type */
 } pcap_hdr_t;
 
+typedef struct pcap_packet_hdr_s {
+  guint32 ts_seconds;
+  guint32 ts_fraction;
+  guint32 captured_packet_length;
+  guint32 original_packet_length;
+} pcap_packet_hdr_t;
+
 char * INPUT = "synflood.pcap";
 
 void main(){
@@ -24,12 +31,18 @@ void main(){
   fd = open(INPUT, O_RDONLY);
 
   size_t header_size = sizeof(pcap_hdr_t);
-  unsigned char * buf = calloc(1, header_size);
-  read(fd, buf, header_size);
+  size_t packet_header_size = sizeof(pcap_packet_hdr_t);
+
+  unsigned char * buf = calloc(1, header_size + packet_header_size);
+  read(fd, buf, header_size + packet_header_size);
   //for (size_t i = 0; i < header_size; ++i) {
   //  printf("%02x ", buf[i]);
   //}
 
   pcap_hdr_t * global = (pcap_hdr_t *) buf;
-  printf("%x", global->magic_number);
+  printf("magic number %x\n", global->magic_number);
+  printf("snapshot length %d\n", global->snaplen);
+
+  pcap_packet_hdr_t * packet = (pcap_packet_hdr_t *) (buf + packet_header_size);
+  printf("cap length %d\n", packet->captured_packet_length);
 }
